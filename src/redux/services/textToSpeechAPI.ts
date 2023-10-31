9fd3de7b400adc6b3a110e5ca60def263a0eb5c5
Tania Troshchuk <troschuk95@gmail.com>
2023-10-31 16:00:15 +0200

    removed comments in textToSpeechAPI

--- src/redux/services/textToSpeechAPI.ts
+++ src/redux/services/textToSpeechAPI.ts
@@ -1,4 +1,3 @@
-// import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/dist/query/react";
 import { responseHandler } from '../responseHandler'
 import { clientWithToken } from '../axiosClients'
 import { ITextToSpeech } from '../../data/types'
@@ -11,22 +10,3 @@ export const getSpeech = async (data: ITextToSpeech): Promise<Blob> => {
 
   return response.data
 }
-
-// export const textToSpeechAPI = createApi({
-//   reducerPath: 'api/text-to-speech',
-//   baseQuery: fetchBaseQuery({ baseUrl: '/' }),
-//   tagTypes: ['Text-to-speech'],
-//   endpoints: (builder) => ({
-//     getSpeech: builder.mutation<Blob, ITextToSpeech>({
-//       queryFn: async (data: ITextToSpeech) => {
-//         return await responseHandler(clientWithToken.post(
-//           'api/text-to-speech',
-//           data,
-//           { responseType: 'blob' })
-//         )
-//       }
-//     })
-//   }),
-// })
-
-// export const { useGetSpeechMutation } = textToSpeechAPI
