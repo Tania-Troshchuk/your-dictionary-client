a28aaeba9fe9f40250c49be9711142f8b574c2ed
Tania Troshchuk <troschuk95@gmail.com>
2023-11-02 18:46:09 +0200

    added basename to router

--- src/main.tsx
+++ src/main.tsx
@@ -10,7 +10,7 @@ import store from './redux/store.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/your-dictionary-client">
       <Provider store={store}>
         <App />
       </Provider>
